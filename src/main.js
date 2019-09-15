efe0f837242ba1d07a948f3b006e290a0fcaa403
emilie.yu <emilie.yu@unity3d.com>
2019-09-15 17:34:08 +0200

    use geodesic distance to control growth, add GUI

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'dat.gui';
 
 import Vector from '../geometry-processing-js/node/linear-algebra/vector';
+import DenseMatrix from '../geometry-processing-js/node/linear-algebra/dense-matrix';
 import { Geometry } from '../geometry-processing-js/node/core/geometry';
 import MeshIO from '../geometry-processing-js/node/utils/meshio';
 import { Mesh } from '../geometry-processing-js/node/core/mesh';
+import HeatMethod from '../geometry-processing-js/node/projects/geodesic-distance/heat-method';
 // const Mesh = MeshModule[0]; // Mesh class
 // const Geometry = GeometryModule[0]; // Geometry class
 
@@ -15,6 +18,17 @@ import './style.css';
 const BEIGE = new Vector(1.0, 0.9, 0.9);
 const ORANGE = new Vector(1.0, 0.5, 0.0);
 const GREEN = new Vector(0.0, 1.0, 0.3);
+
+// GUI
+let sceneState = function() {
+  this.growStep = function() { grow() };
+  this.growScale = 0.05;
+};
+
+let params = new sceneState();
+const gui = new dat.GUI();
+gui.add( params, 'growStep' );
+gui.add( params, 'growScale' );
 
 // Init THREE.js scene
 const canvas = document.createElement('canvas');
@@ -48,33 +62,6 @@ let geometry = new Geometry(mesh, polygonSoup["v"]);
 // Add colors
 let colors = new Array(mesh.vertices.length);
 colors.fill(BEIGE);
-
-// Grow edge vertices
-let boundaryFace = mesh.boundaries[0];
-
-for (let v of boundaryFace.adjacentVertices()) {
-  colors[v.index] = ORANGE;
-  // Find an inner edge
-  let innerEdge;
-  for (let e of v.adjacentEdges()) {
-    innerEdge = e.halfedge;
-    if (!innerEdge.onBoundary && !innerEdge.twin.onBoundary)
-      break;
-  }
-  let innerVertex = innerEdge.vertex;
-  if (innerVertex.index == v.index) {
-    innerVertex = innerEdge.twin.vertex;
-  };
-  colors[innerVertex.index] = GREEN;
-
-  // Translate edge vertex along inner edge
-  let vPos = geometry.positions[v.index];
-  let iPos = geometry.positions[innerVertex.index];
-  let dir = vPos.minus(iPos);
-  dir.normalize();
-  dir.scaleBy(0.1);
-  geometry.positions[v.index] = vPos.plus(dir);
-}
 
 // Create THREE.js geometry object
 let threeGeometry = new THREE.BufferGeometry();
@@ -114,26 +101,113 @@ threeGeometry.addAttribute("color", new THREE.BufferAttribute(threeColors, 3));
 
 // Create material
 let threeMaterial = new THREE.MeshBasicMaterial(
-	{ vertexColors: THREE.VertexColors }
-	);
+  { 
+    vertexColors: THREE.VertexColors,
+    wireframe: true,
+  }
+);
 
 // Create THREEjs mesh
 let threeMesh = new THREE.Mesh(threeGeometry, threeMaterial);
 scene.add(threeMesh);
 
-// Create wireframe
-let wireframe = new THREE.WireframeGeometry( threeGeometry );
+// // Create wireframe
+// let wireframe = new THREE.WireframeGeometry( threeGeometry );
 
-let line = new THREE.LineSegments( wireframe );
-line.material.color = new THREE.Color("#ff0000");
-line.material.depthTest = false;
-line.material.opacity = 0.5;
-line.material.transparent = true;
-scene.add(line);
+// let line = new THREE.LineSegments( wireframe );
+// line.material.color = new THREE.Color("#ff0000");
+// line.material.depthTest = false;
+// line.material.opacity = 0.5;
+// line.material.transparent = true;
+// scene.add(line);
 
 
 // Render scene
-render();
+animate();
+
+function computeGeodesics() {
+  // initialize geodesics in heat
+  let V = mesh.vertices.length;
+  // Vector containing sources
+  let delta = DenseMatrix.zeros(V, 1);
+
+  // Grow edge vertices
+  let boundaryFace = mesh.boundaries[0];
+
+  for (let v of boundaryFace.adjacentVertices()) {
+    colors[v.index] = ORANGE;
+    // Find an inner edge
+    let innerEdge;
+    for (let e of v.adjacentEdges()) {
+      innerEdge = e.halfedge;
+      if (!innerEdge.onBoundary && !innerEdge.twin.onBoundary)
+        break;
+    }
+    let innerVertex = innerEdge.vertex;
+    if (innerVertex.index == v.index) {
+      innerVertex = innerEdge.twin.vertex;
+    };
+    colors[innerVertex.index] = GREEN;
+
+    delta.set(1, v.index, 0);
+  }
+
+  let heatMethod = new HeatMethod(geometry);
+
+  return heatMethod.compute(delta);
+}
+
+function grow() {
+  let phi = computeGeodesics();
+  let maxPhi = 0;
+  for (let i = 0; i < phi.nRows(); i++) {
+    maxPhi = Math.max(phi.get(i, 0), maxPhi);
+  }
+
+  for (let e of mesh.edges) {
+    let vA = e.halfedge.vertex;
+    let vB = e.halfedge.twin.vertex;
+    let phiA = phi.get(vA.index, 0);
+    let phiB = phi.get(vB.index, 0);
+
+    // Grow towards slower phi
+    if (phiA < phiB) {
+      [vA, vB] = [vB, vA];
+    }
+
+    let growthDir = geometry.positions[vB.index].minus(geometry.positions[vA.index]);
+    growthDir.normalize();
+
+    // Scale growth depending on geodesic distance
+    let geodesicScale = maxPhi - (phiA + phiB) / 2;
+    growthDir.scaleBy(geodesicScale * params.growScale);
+    geometry.positions[vB.index] = geometry.positions[vB.index].plus(growthDir);
+  }
+
+  updateGeometry();
+}
+
+function updateGeometry() {
+  for (let v of mesh.vertices) {
+    let i = v.index;
+    let position = geometry.positions[i];
+    threeGeometry.attributes.position.setXYZ( i, position.x, position.y, position.z );
+    
+  };
+
+  threeGeometry.attributes.position.needsUpdate = true;
+  threeGeometry.computeBoundingSphere();
+}
+
+function animate() {
+
+  // grow();
+
+  render();
+
+  // Next frame
+  requestAnimationFrame(animate);
+}
 
 function render() {
   if (resizeRendererToDisplaySize(renderer)) {
@@ -145,8 +219,7 @@ function render() {
 	controls.update();
 
   renderer.render(scene, camera);
-  // Next frame
-  requestAnimationFrame(render);
+
 }
 
 
