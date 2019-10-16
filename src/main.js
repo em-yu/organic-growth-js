0f86276ec7866a012c87ce5c0b5fb0891131dd95
emilie.yu <emilie.yu@unity3d.com>
2019-10-16 18:03:04 +0200

    refactor to separate scene related code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
 
 import Vector from '../geometry-processing-js/node/linear-algebra/vector';
@@ -9,6 +7,7 @@ import { Mesh } from '../geometry-processing-js/node/core/mesh';
 
 import smallDisk from '../geometry-processing-js/input/small_disk.obj';
 
+import Scene from './scene';
 import DiscreteShells from './discrete-shells';
 import ParticleCollisions from './particle-collisions';
 import EdgeBasedGrowth from './edge-based-growth';
@@ -24,10 +23,11 @@ let growCounter = 0;
 
 // GUI
 let sceneState = function() {
+  this.playGrowth = false;
   this.growStep = function() { grow() };
   this.growSteps = function() { growSteps() };
   this.nSteps = 10;
-  this.growScale = 0.05;
+  // this.growScale = 0.05;
   this.collisionDetection = true;
   this.energyMin = true;
   this.kb = 5.0;
@@ -39,37 +39,26 @@ let sceneState = function() {
 
 let params = new sceneState();
 const gui = new dat.GUI();
-gui.add( params, 'growStep' );
-gui.add( params, 'growSteps' );
-gui.add( params, 'nSteps' );
-gui.add( params, 'growScale' );
-gui.add( params, 'collisionDetection' );
-gui.add( params, 'energyMin' );
-gui.add( params, 'kb' );
-gui.add( params, 'timeStep' );
-gui.add( params, 'nIter' );
+gui.add( params, 'playGrowth');
 gui.add( params, 'wireframe' );
-gui.add( params, 'edgeSize' );
+
+if (process.env.NODE_ENV === 'development') {
+  gui.add( params, 'growStep' );
+  gui.add( params, 'growSteps' );
+  gui.add( params, 'nSteps' );
+  // gui.add( params, 'growScale' );
+  gui.add( params, 'collisionDetection' );
+  gui.add( params, 'energyMin' );
+  gui.add( params, 'kb' );
+  gui.add( params, 'timeStep' );
+  gui.add( params, 'nIter' );
+
+  gui.add( params, 'edgeSize' );
+}
 
 // Init THREE.js scene
-const canvas = document.createElement('canvas');
-document.body.appendChild(canvas);
-
-var renderer = new THREE.WebGLRenderer({
-  canvas
-});
-
-var scene = new THREE.Scene();
-const fov = 45.0;
-const aspect = canvas.clientWidth / canvas.clientHeight;
-const near = 0.1;
-const far = 100;
-const eyeZ = 3.5;
-
-var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = eyeZ;
-var controls = new OrbitControls( camera, renderer.domElement );
-controls.update();
+const scene = new Scene(MAX_POINTS);
+scene.init();
 
 
 // Build mesh from polygon soup
@@ -82,80 +71,11 @@ console.log(mesh);
 let geometry = new Geometry(mesh, polygonSoup["v"], MAX_POINTS);
 raiseEdge(0.1);
 
-// Add colors
-let colors = new Array(mesh.vertices.length);
-colors.fill(BEIGE);
+scene.updateGeometry(mesh, geometry);
 
-// Create THREE.js geometry object
-let threeGeometry = new THREE.BufferGeometry();
+// let sources = getGrowthSources(4);
 
-// Fill position and color buffers
-// let V = mesh.vertices.length;
-let threePositions = new Float32Array(MAX_POINTS * 3);
-let threeNormals = new Float32Array(MAX_POINTS * 3);
-// let threeColors = new Float32Array(MAX_POINTS * 3);
-for (let v of mesh.vertices) {
-	let i = v.index;
-
-	let position = geometry.positions[i];
-	threePositions[3 * i + 0] = position.x;
-	threePositions[3 * i + 1] = position.y;
-  threePositions[3 * i + 2] = position.z;
-
-  // Angle weighted normals
-  let normal = geometry.vertexNormalAngleWeighted(v);
-	threeNormals[3 * i + 0] = normal.x;
-	threeNormals[3 * i + 1] = normal.y;
-  threeNormals[3 * i + 2] = normal.z;
-  
-  // let color = colors[i];
-  // threeColors[3 * i + 0] = color.x;
-  // threeColors[3 * i + 1] = color.y;
-  // threeColors[3 * i + 2] = color.z;
-};
-
-let indices = new Uint32Array(MAX_POINTS * 3);
-console.log(geometry.positions)
-
-for (let i = 0; i < geometry.indices.length; i++) {
-  indices[i] = geometry.indices[i];
-};
-
-// Set geometry
-threeGeometry.setIndex(new THREE.BufferAttribute(indices, 1));
-threeGeometry.addAttribute('position', new THREE.BufferAttribute(threePositions, 3));
-threeGeometry.addAttribute('normal', new THREE.BufferAttribute(threeNormals, 3));
-// threeGeometry.addAttribute("color", new THREE.BufferAttribute(threeColors, 3));
-
-// Create material
-let threeMaterial = new THREE.MeshBasicMaterial(
-  {
-    wireframe: true,
-    side: THREE.DoubleSide,
-  }
-);
-
-let normalMat = new THREE.MeshPhongMaterial( {
-  side: THREE.DoubleSide,
-  wireframe: params.wireframe,
-} );
-
-// Create THREEjs mesh
-let threeMesh = new THREE.Mesh(threeGeometry, normalMat);
-scene.add(threeMesh);
-
-// let pointColor = new THREE.Color( 'rgb(231, 76, 60)' );
-var pointLight = new THREE.PointLight( 0xff0000, 1, 100 );
-pointLight.position.set( 20, 20, 20 );
-scene.add( pointLight );
-
-var ambientLight = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( ambientLight );
-
-let sources = getGrowthSources(5);
-console.log(sources);
-
-let GrowthProcess = new EdgeBasedGrowth(geometry, 1.5, 8.0, params.edgeSize * 2.0);
+let growthProcess = new EdgeBasedGrowth(geometry, 1.5, 4.0, params.edgeSize * 2.0);
 
 // Render scene
 animate();
@@ -167,7 +87,7 @@ function raiseEdge(z) {
     if (z !== undefined)
       up = z;
     else
-      up = 2 * (Math.random() - 0.5) / 50;
+      up = Math.random() / 100;
     geometry.positions[v.index].incrementBy(new Vector(0, 0, up));
   }
 }
@@ -211,8 +131,8 @@ function growSteps() {
 }
 
 function grow() {
-
-  GrowthProcess.growEdges();
+  // raiseEdge();
+  growthProcess.growEdges();
 
   // Store positions before energy minimization
   const positions0 = {};
@@ -229,7 +149,7 @@ function grow() {
 
   balanceMesh();
   
-  updateGeometry();
+  scene.updateGeometry(mesh, geometry);
 
   growCounter++;
   console.log("Grow step: " + growCounter)
@@ -282,64 +202,13 @@ function integrateForces(X0, X) {
   }
 }
 
-
-function updateGeometry() {
-  for (let v of mesh.vertices) {
-    let i = v.index;
-    let position = geometry.positions[i];
-    threeGeometry.attributes.position.setXYZ( i, position.x, position.y, position.z );
-
-    // Angle weighted normals
-    let normal = geometry.vertexNormalAngleWeighted(v);
-    threeGeometry.attributes.normal.setXYZ(i, normal.x, normal.y, normal.z);
-  };
-  // This needs to be done only if there was edge splitting
-  for (let i = 0; i < geometry.indices.length; i++) {
-    indices[i] = geometry.indices[i];
-  };
-
-  threeGeometry.index.set(indices);
-  threeGeometry.attributes.position.needsUpdate = true;
-  threeGeometry.attributes.normal.needsUpdate = true;
-  threeGeometry.index.needsUpdate = true;
-  threeGeometry.computeBoundingSphere();
-}
-
 function animate() {
 
-  // grow();
+  if (params.playGrowth)
+    grow();
 
-  render();
+  scene.render(params);
 
   // Next frame
   requestAnimationFrame(animate);
-}
-
-function render() {
-  if (resizeRendererToDisplaySize(renderer)) {
-    const canvas = renderer.domElement;
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
-	}
-	
-  controls.update();
-  
-  // Set wireframe mode
-  threeMesh.material.wireframe = params.wireframe;
-
-  renderer.render(scene, camera);
-
-}
-
-
-// Responsive canvas
-function resizeRendererToDisplaySize(renderer) {
-  const canvas = renderer.domElement;
-  const width = canvas.clientWidth;
-  const height = canvas.clientHeight;
-  const needResize = canvas.width !== width || canvas.height !== height;
-  if (needResize) {
-    renderer.setSize(width, height, false);
-  }
-  return needResize;
 }
