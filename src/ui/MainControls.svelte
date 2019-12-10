a1a2c5ae7afb2db5485d1b2652ed2efcfff5e512
emilie.yu <emilie.yu@unity3d.com>
2019-12-10 09:10:47 +0100

    prevent mouse select text in UI

--- src/ui/MainControls.svelte
+++ src/ui/MainControls.svelte
@@ -40,6 +40,10 @@
 		color: #DADADA;
 		display: flex;
 		flex-direction: row;
+		-webkit-user-select: none; /* Safari */        
+		-moz-user-select: none; /* Firefox */
+		-ms-user-select: none; /* IE10+/Edge */
+		user-select: none; /* Standard */
 	}
 
 	.main-controls .material-icons {
--- src/ui/Parameter.svelte
+++ src/ui/Parameter.svelte
@@ -21,6 +21,10 @@
 <style>
 	.parameter {
 		padding: 5px 20px;
+		-webkit-user-select: none; /* Safari */        
+		-moz-user-select: none; /* Firefox */
+		-ms-user-select: none; /* IE10+/Edge */
+		user-select: none; /* Standard */
 	}
 
 	.label{
